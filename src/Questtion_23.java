bf331e249f4d40511b4cfab8f261d1533f8fb368
Neeraj <485neerajkumar@gmail.com>
2023-07-15 23:15:48 +0530

    final commit

--- src/Questtion_23.java
+++ src/Questtion_23.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 
-public class Question_9 {
+public class Questtion_23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i=0;i<n;i++){
-            for (int space=0;space<n-i-1;space++){
+            for(int space=0;space<n-i-1;space++){
                 System.out.print("  ");
             }
-            for(int star=1;star<=i*2+1;star++){
-                System.out.print("* ");
+            for(int star=0;star<i*2+1;star++){
+                System.out.print("1 ");
             }
             System.out.println();
         }
--- src/Questtion_30.java
+++ src/Questtion_30.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-public class Question_9 {
+public class Questtion_30 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         for(int i=0;i<n;i++){
-            for (int space=0;space<n-i-1;space++){
-                System.out.print("  ");
-            }
-            for(int star=1;star<=i*2+1;star++){
-                System.out.print("* ");
+            for(int j=n;j>=1;j--){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
